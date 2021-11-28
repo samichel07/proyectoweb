504c4cb2f681ba1085a0847e6b16e598f9db8ea1
samichel07 <samichel1997girl@gmail.com>
2021-11-28 00:27:31 -0600

    LOGIN,REGISTRO,BASE y NG-MODEL
    Modificaciones login.html
    Se agrego login.php junto con sus funciones
    se agrego angular ng-model
    se agrego la base para el login

---
+++ js/funciones.js
@@ -0,0 +1,14 @@
+function agregardatos(usuario,correoo,contrasenaa){
+    cadena="usuario="+ usuario + "&correoo=" + correoo+ "&contrasenaa=" + contrasenaa ;
+    $.ajax({
+        type:"POST",
+        url:"insertar_registro.php",
+        data: cadena
+        
+    });
+}
+
+
+
+
+
